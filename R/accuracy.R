7df2f57fd034d6385dbae08c623baac55d37357e
martinschobben <m.a.n.schobben@uu.nl>
2021-03-03 20:01:37 +0100

    roxygen

--- R/accuracy.R
+++ R/accuracy.R
@@ -1,9 +1,17 @@
 #' Accuracy of model classification
 #'
-#' @return Numeric vector
+#' This function can be used in \code{dplyr::\link[dplyr:summarise]{summarise}}
+#' to calculate the accuracy of model classification for the intra- and inter-
+#' isotope test.
+#'
+#' @param x Numeric value for the isotope value of the synthetic value.
+#' @param y p value.
+#' @param ntot Numeric for total number of model runs.
+#'
+#' @return Numeric vector for accuracy.
 #'
 #' @export
-acc_fun <- function(x , y, ntot){
+acc_fun <- function(x, y, ntot){
   if(x != 0) return(sum(y < 0.001) / ntot)
   if(x == 0) return(1 - (sum(y < 0.001) / ntot))
 }
