bb6e3980c056ad2bb5e837d9ef478ceff05ff372
martinschobben <m.a.n.schobben@uu.nl>
2021-03-03 20:04:08 +0100

    roxygen update and copy figures

--- R/render_paper.R
+++ R/render_paper.R
@@ -1,14 +1,24 @@
 #' Render the paper
 #'
-#' \code{render_paper()} is a wrapper around \code{rmarkdown::render_site} which
-#' renders the paper of this study.
+#' \code{render_paper()} is a wrapper around
+#' \code{rmarkdown::\link[rmarkdown:rendersite]{rendersite}} which renders the
+#' paper of this study.
 #'
+#' @param title Character string for title of rendered document.
+#' @param output_dir Character string for directory containing the rendered
+#' document.
+#' @param copy_figures Logical wether to copy figures to directory
+#' (default = TRUE).
 #' @param on_build Logical whether download is loaded during build
 #' (default = FALSE).
 #'
+#' @return Rendered documents including a tex, docx and pdf file. Depending on
+#' the argument \code{copy_figures} the call returns figures in png format
+#' within its own directory (figures).
 #' @export
 render_paper <- function(title = "Schobbenetal_SIMS_method",
                          output_dir = "/home/amandus/Documents/work/manuscripts/Schobbenetal_SIMS_method",
+                         copy_figures = TRUE,
                          on_build = TRUE){
 
   if (on_build) {
@@ -21,11 +31,18 @@ render_paper <- function(title = "Schobbenetal_SIMS_method",
   cat(
     paste0("book_filename: \"",title,"\""),
     "delete_merged_file: true",
-    "rmd_files: [\"index.Rmd\", \"main.Rmd\", \"references.Rmd\"]",
+    "rmd_files: [\"index.Rmd\", \"main.Rmd\", \"SI.Rmd\", \"references.Rmd\"]",
     paste0("output_dir: ", output_dir) ,
+    # "clean: [\"\"]",
     file = paste(path, "_bookdown.yml", sep = "/"),
     sep = "\n"
     )
-
+  # render paper
   rmarkdown::render_site(input = path, encoding = 'UTF-8')
+  # copy figures
+  fs::dir_copy(
+    paste(path, "figures/", sep = "/"),
+    paste(output_dir, "figures", sep = "/"),
+    overwrite = TRUE
+    )
 }
