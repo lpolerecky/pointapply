2c7d2e48d7ee80909ffa36922c7c659f507d2783
martinschobben <m.a.n.schobben@uu.nl>
2021-03-03 20:03:25 +0100

    roxygen update

--- R/raster_IC.R
+++ R/raster_IC.R
@@ -2,30 +2,34 @@
 #'
 #' \code{gg_cnts} Raster images based on the ratio of ion counts.
 #'
-#' @param .data Dataframe.
-#' @param .ion1 A character string constituting the rare isotope ("13C").
-#' @param .ion2 A character string constituting the common isotope ("12C").
+#' @param IC Ion count dataframe.
+#' @param ion1 A character string constituting the rare isotope ("13C").
+#' @param ion2 A character string constituting the common isotope ("12C").
 #' @param ttl Character string or expression for plot title.
 #' @param viri Character string selecting the viriditas color scheme.
+#' @param height Pixels of height.
+#' @param width Pixels of width.
+#' @param grid_cell Pixel size of grid_cells (one side of the square).
 #' @param scaler Numeric converting the pixels to metric dimension of
 #' measurement (default is the conversion used in this study).
 #' @param compilation Logical whether to plot all three dimensions according to
-#' the sketch ...
+#' the grid layout
+#' (see \code{pointapply::\link[pointapply:gg_sketch]{gg_sketch}}).
 #'
-#' @return \code{\link[ggplot2:ggplot]{ggplot}}.
+#' @return \code{ggplot2::\link[ggplot2:ggplot]{ggplot}}.
 #'
 #' @export
-gg_cnts <- function(.data, ion1, ion2, ttl, viri, height = 256, width = 256,
+gg_cnts <- function(IC, ion1, ion2, ttl, viri, height = 256, width = 256,
                     grid_cell = 64, scaler = 40 / 256, compilation = FALSE) {
 
   Xt1 <- rlang::parse_expr(paste("Xt.pr", ion1, sep = "."))
   Xt2 <- rlang::parse_expr(paste("Xt.pr", ion2, sep = "."))
 
-  .data <- point::cov_R(.data, c(ion1, ion2), file.nm, sample.nm, dim_name.nm,
-                        height, width, grid.nm) %>%
+  IC <- point::cov_R(IC, c(ion1, ion2), file.nm, sample.nm, dim_name.nm,
+                     height, width, grid.nm) %>%
     mutate(R = !!Xt1 / !!Xt2)
 
-  ggplot(.data, aes(x = width, y = height)) +
+  ggplot(IC, aes(x = width, y = height)) +
     geom_raster(aes(fill = R)) +
     scale_y_continuous(
       expression("height"~"("*mu*"m)"),
